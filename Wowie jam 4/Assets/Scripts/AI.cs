e239835a6ee2642902242373cc203c09d4a8fad4
slapota <arnost.spalensky@gmail.com>
2022-08-13 20:49:45 +0200

    rejkást

--- Wowie jam 4/Assets/Scripts/AI.cs
+++ Wowie jam 4/Assets/Scripts/AI.cs
@@ -5,45 +5,24 @@ using UnityEngine;
 public class AI : MonoBehaviour
 {
     public List<Command> commands = new List<Command>();
-    public GameObject runButton;
-    public Transform canvas;
+    public GameObject runButton1, runButton2;
     int index;
     public float waitTime = 0.1f;
-
-
-    public void Run()
+    
+    public void SetStartPos()
     {
         transform.position = Vector3.zero;
         transform.rotation = new Quaternion(0, 0, 0, 0);
         index = 0;
-        StartCoroutine(Execute());
-        runButton.SetActive(false);
     }
-    public void AddCommand(Command command)
+    
+    public IEnumerator Execute()
     {
-        Command instance = Instantiate(command, canvas);
-        instance.ai = transform;
-        commands.Add(instance);
-        instance.transform.localPosition = new Vector2(-485, 450 - 50*commands.IndexOf(instance));
-    }
-    public void DeleteCommand()
-    {
-        Destroy(commands[commands.Count - 1].gameObject);
-        commands.RemoveAt(commands.Count-1);
-    }
-    IEnumerator Execute()
-    {
+        if (index >= commands.Count) index = 0;
         commands[index].Function();
         yield return new WaitForSeconds(waitTime);
         waitTime = 0.1f;
-        if(index< commands.Count - 1)
-        {
-            index++;
-            StartCoroutine(Execute());
-        }
-        else
-        {
-            runButton.SetActive(true);
-        }
+        index++;
+        StartCoroutine(Execute());
     }
 }
---
+++ Wowie jam 4/Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ButtonManager : MonoBehaviour
+{
+    public Transform canvas;
+    [SerializeField] AI ai;
+
+    public void Open_Close_Console(GameObject ob)
+    {
+        ob.SetActive(true);
+        EventSystem.current.currentSelectedGameObject.GetComponentInParent<Canvas>().gameObject.SetActive(false);
+    }
+    public void DeleteCommand()
+    {
+        Destroy(ai.commands[ai.commands.Count - 1].gameObject);
+        ai.commands.RemoveAt(ai.commands.Count - 1);
+    }
+    public void Run()
+    {
+        ai.SetStartPos();
+        StartCoroutine(ai.Execute());
+        ai.runButton1.SetActive(false);
+        ai.runButton2.SetActive(false);
+    }
+    public void AddCommand(Command command)
+    {
+        Command instance = Instantiate(command, canvas);
+        instance.ai = transform;
+        ai.commands.Add(instance);
+        instance.transform.localPosition = new Vector2(-485, 450 - 50 * ai.commands.IndexOf(instance));
+    }
+    public bool ifBlocking()
+    {
+        int layerMask = 1 << 8;
+        layerMask = ~layerMask;
+        RaycastHit hit;
+        if(Physics.Raycast(ai.gameObject.transform.position, ai.gameObject.transform.TransformDirection(Vector3.forward), out hit, 2f, layerMask))
+    }
+}
